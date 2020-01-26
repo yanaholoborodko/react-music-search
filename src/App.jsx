cabdf3c4abf8c5007d70974799296a5291847c6d
Yana <holoborodkoyana97@gmail.com>
2020-01-26 19:03:02 +0100

    Mechanizm for fetching data from Spotify

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@ class App extends Component {
   }
 
   search() {
-    console.log('state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search';
+    const FETCH_URL =  `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL, { method: 'GET', headers: { 'Authorization': `Bearer ${process.env.REACT_APP_SPOTIFY_TOKEN}` } })
+        .then(response => response.json())
+        .then(json => console.log('json', json));
   }
 
   render() {
@@ -26,6 +30,11 @@ class App extends Component {
               placeholder="Search for an artist"
               value={this.state.query}
               onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if(event.key === 'Enter') {
+                  this.search();
+                }
+              }}
             />
             <InputGroup.Append onClick={() => this.search()}>
             <Button><FaSearch/></Button>
