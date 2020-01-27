a43a8ac8792ea81929273ecfdf0fd318a38e021b
Yana <holoborodkoyana97@gmail.com>
2020-01-27 20:56:10 +0100

    Fetching and displaying data about artist

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,32 @@ import React, {Component} from "react";
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 import { FaSearch } from 'react-icons/fa';
+import Profile from './Profile'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      artist: null
     }
   }
 
   search() {
     const BASE_URL = 'https://api.spotify.com/v1/search';
-    const FETCH_URL =  `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
-    fetch(FETCH_URL, { method: 'GET', headers: { 'Authorization': `Bearer ${process.env.REACT_APP_SPOTIFY_TOKEN}` } })
-        .then(response => response.json())
-        .then(json => console.log('json', json));
+    let FETCH_URL =  `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    const TRACKS_URL = 'https://api.spotify.com/v1/artists/';
+
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: { 'Authorization': `Bearer ${process.env.REACT_APP_SPOTIFY_TOKEN}` }
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log(artist);
+      this.setState({artist});
+    });
   }
 
   render() {
@@ -42,13 +53,19 @@ class App extends Component {
           </InputGroup>
 
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
-        <div className="Gallery">
-          Gallery
-        </div>
+        {
+          this.state.artist !== null
+          ? <div>
+                <Profile
+                    artist={this.state.artist}
+                />
+                <div className="Gallery">
+                  Gallery
+                </div>
+              </div>
+          : <div></div>
+        }
+
       </div>
     )
   }
